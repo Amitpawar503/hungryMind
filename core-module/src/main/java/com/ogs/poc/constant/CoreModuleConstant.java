89afbc22c4ee23a730ca87eb14a3c7f0f6c53493
Pawar <amit.pawar@scientificgames.com>
2018-10-10 17:39:50 +0530

    abc

--- core-module/src/main/java/com/ogs/poc/constant/CoreModuleConstant.java
+++
@@ -1,8 +0,0 @@
-package com.ogs.poc.constant;
-
-public class CoreModuleConstant {
-
-	public static final String REQUESTTYPE = "getAccount";
-	public static final String ACCOUNTDETAIL_URL = "/operatorwalletplatform/path/to/wallet/tokenID=%s";
-
-}
--- core-module/src/main/java/com/ogs/poc/repository/AccountDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ogs.poc.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.ogs.poc.beans.AccountDetails;
-
-public interface AccountDetailsRepository extends MongoRepository<AccountDetails, Long> {
-
-}
